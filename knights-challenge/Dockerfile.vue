7bdbf03de69310cc2d60bf360842212834efb6a9
Renato Carvalho <renato@resolvendobug.com.br>
2024-03-01 11:28:26 -0300

    Add Docker Compose configuration and Dockerfiles for Vue.js and Nest.js

---
+++ knights-challenge/Dockerfile.vue
@@ -0,0 +1,15 @@
+# Dockerfile.vue
+FROM node:lts-alpine
+
+# instalar dependências simples
+RUN apk add --no-cache git
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm", "run", "serve"]
