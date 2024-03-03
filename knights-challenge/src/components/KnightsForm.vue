a5238b77c51fe3c17830c972b3e32a131b373268
Renato Carvalho <renato@resolvendobug.com.br>
2024-03-02 23:30:19 -0300

    Refactor code to improve performance and readability

--- knights-challenge/src/components/KnightsForm.vue
+++ knights-challenge/src/components/KnightsForm.vue
@@ -1,26 +1,76 @@
 <template>
   <div class="container">
     <b-form-group id="labelarma1" label="Classe:" label-for="arma">
-      <b-img
-        v-for="(image, index) in classesImages"
-        :key="index"
-        :src="image"
-        width="155"
-        height="155"
-        rounded
-        mr-1
-        alt="Image 1"
-        @click="selectClassesImage(index)"
-        :class="{ 'selected-image': selectedClassesImage === index }"
-      ></b-img>
+      <b-row>
+        <b-col v-for="(classe, index) in classesImages" :key="index">
+          <b-img
+            :src="classe.image"
+            width="155"
+            height="155"
+            rounded
+            mr-1
+            alt="Image 1"
+            @click="selectClassesImage(index)"
+            :class="{ 'selected-image': selectedClassesImage === index }"
+            class="center-text"
+          ></b-img>
+          <p class="center-text">{{ classe.name }}</p>
+        </b-col>
+      </b-row>
+
+      <b-row>
+            <b-col>
+              <b-form-group
+                id="labelarma1"
+                label="Arma esquerda:"
+                label-for="arma"
+              >
+                <b-img
+                  v-for="(image, index) in weaponImages"
+                  :key="index"
+                  :src="image"
+                  width="75"
+                  height="75"
+                  rounded
+                  mr-1
+                  alt="Image 1"
+                  @click="selectWeaponEsquerda(index)"
+                  :class="{ 'selected-image': selectedWeaponEsquerda === index }"
+                ></b-img>
+              </b-form-group>
+            </b-col>
+
+            <b-col>
+              <b-form-group
+                id="labelarma1"
+                label="Arma direita:"
+                label-for="arma"
+              >
+                <b-img
+                  v-for="(image, index) in weaponImages"
+                  :key="index"
+                  :src="image"
+                  width="75"
+                  height="75"
+                  rounded
+                  mr-1
+                  alt="Image 1"
+                  @click="selectWeaponDireita(index)"
+                  :class="{ 'selected-image': selectedWeaponDireita === index }"
+                ></b-img>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+
     </b-form-group>
     <b-row>
-      <b-col
-        ><b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-col>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
           <b-form-group id="labelnome" label="Nome:" label-for="nome">
             <b-form-input
               id="nome"
-              v-model="form.nome"
+              v-model="character.nome"
               type="text"
               placeholder="Nome do Guerreiro"
               required
@@ -30,33 +80,19 @@
           <b-form-group id="labelapelido" label="Apelido" label-for="apelido">
             <b-form-input
               id="apelido"
-              v-model="form.apelido"
+              v-model="character.apelido"
               placeholder="Apelido do Guerreiro"
               required
             ></b-form-input>
           </b-form-group>
-
-          <b-form-group id="labelarma1" label="Arma:" label-for="arma">
-            <b-img
-              v-for="(image, index) in weaponImages"
-              :key="index"
-              :src="image"
-              width="75"
-              height="75"
-              rounded
-              mr-1
-              alt="Image 1"
-              @click="selectWeaponImage(index)"
-              :class="{ 'selected-image': selectedWeaponImage === index }"
-            ></b-img>
-          </b-form-group>
+          
 
           <b-form-checkbox> </b-form-checkbox>
 
           <b-button type="submit" variant="primary">Submit</b-button>
           <b-button type="reset" variant="danger">Reset</b-button>
-        </b-form></b-col
-      >
+        </b-form>
+      </b-col>
       <b-col> {{ character }}</b-col>
     </b-row>
   </div>
@@ -68,6 +104,8 @@ export default {
   data() {
     return {
       character: {
+        nome: null,
+        apelido: null,
         attributes: {
           strength: 0,
           dexterity: 0,
@@ -76,15 +114,16 @@ export default {
           wisdom: 0,
           charisma: 0,
         },
+        weapons: [],
         keyAttribute: null,
       },
 
       classesImages: [
-        "/img/classes/arqueiro.jpeg",
-        "/img/classes/barbaro.jpeg",
-        "/img/classes/curandeiro.jpeg",
-        "/img/classes/mago.jpeg",
-        "/img/classes/monge.jpeg",
+        { image: "/img/classes/arqueiro.jpeg", name: "Arqueiro" },
+        { image: "/img/classes/barbaro.jpeg", name: "Bárbaro" },
+        { image: "/img/classes/curandeiro.jpeg", name: "Curandeiro" },
+        { image: "/img/classes/mago.jpeg", name: "Mago" },
+        { image: "/img/classes/monge.jpeg", name: "Monge" },
       ],
       selectedClassesImage: null,
       weaponImages: [
@@ -93,7 +132,8 @@ export default {
         "/img/weapons/machado.jpeg",
         "/img/weapons/arco.jpeg",
       ],
-      selectedWeaponImage: null,
+      selectedWeaponEsquerda: null,
+      selectedWeaponDireita: null,
       form: {
         email: "",
         name: "",
@@ -115,27 +155,159 @@ export default {
       this.selectedClassesImage = index;
       switch (index) {
         case 0: // Arqueiro
-          this.character.keyAttribute = 'dexterity';
+          this.character.attributes = {
+            strength: 10,
+            dexterity: 20,
+            constitution: 15,
+            intelligence: 10,
+            wisdom: 10,
+            charisma: 10,
+          };
+          this.character.keyAttribute = "dexterity";
           break;
         case 1: // Bárbaro
-          this.character.keyAttribute = 'strength';
+          this.character.attributes = {
+            strength: 20,
+            dexterity: 10,
+            constitution: 20,
+            intelligence: 5,
+            wisdom: 10,
+            charisma: 10,
+          };
+          this.character.keyAttribute = "strength";
           break;
         case 2: // Curandeiro
-          this.character.keyAttribute = 'wisdom';
+          this.character.attributes = {
+            strength: 5,
+            dexterity: 10,
+            constitution: 15,
+            intelligence: 15,
+            wisdom: 20,
+            charisma: 15,
+          };
+          this.character.keyAttribute = "wisdom";
           break;
         case 3: // Mago
-          this.character.keyAttribute = 'intelligence';
+          this.character.attributes = {
+            strength: 5,
+            dexterity: 10,
+            constitution: 10,
+            intelligence: 20,
+            wisdom: 15,
+            charisma: 10,
+          };
+          this.character.keyAttribute = "intelligence";
           break;
         case 4: // Monge
-          this.character.keyAttribute = 'constitution';
+          this.character.attributes = {
+            strength: 15,
+            dexterity: 15,
+            constitution: 20,
+            intelligence: 10,
+            wisdom: 15,
+            charisma: 10,
+          };
+          this.character.keyAttribute = "constitution";
           break;
         default:
+          this.character.attributes = {
+            strength: 0,
+            dexterity: 0,
+            constitution: 0,
+            intelligence: 0,
+            wisdom: 0,
+            charisma: 0,
+          };
           this.character.keyAttribute = null;
       }
     },
-    selectWeaponImage(index) {
-      this.selectedWeaponImage = index;
-      //alert(this.selectedImage);
+    selectWeaponEsquerda(index) {
+      this.selectedWeaponEsquerda = index;
+      switch (index) {
+        case 0: // Espada
+          this.character.weapons[0] = {
+            name: "sword",
+            mod: 3,
+            attr: "strength",
+            equipped: true,
+          };
+          break;
+        case 1: // Faca
+          this.character.weapons[0] = {
+            name: "knife",
+            mod: 2,
+            attr: "dexterity",
+            equipped: true,
+          };
+          break;
+        case 2: // Machado
+          this.character.weapons[0] = {
+            name: "axe",
+            mod: 4,
+            attr: "strength",
+            equipped: true,
+          };
+          break;
+        case 3: // Arco
+          this.character.weapons[0] = {
+            name: "bow",
+            mod: 2,
+            attr: "dexterity",
+            equipped: true,
+          };
+          break;
+        default:
+          this.character.weapons[0] = {
+            name: "",
+            mod: 0,
+            attr: "",
+            equipped: false,
+          };
+      }
+    },
+    selectWeaponDireita(index) {
+      this.selectedWeaponDireita = index;
+      switch (index) {
+        case 0: // Espada
+          this.character.weapons[1] = {
+            name: "sword",
+            mod: 3,
+            attr: "strength",
+            equipped: true,
+          };
+          break;
+        case 1: // Faca
+          this.character.weapons[1] = {
+            name: "knife",
+            mod: 2,
+            attr: "dexterity",
+            equipped: true,
+          };
+          break;
+        case 2: // Machado
+          this.character.weapons[1] = {
+            name: "axe",
+            mod: 4,
+            attr: "strength",
+            equipped: true,
+          };
+          break;
+        case 3: // Arco
+          this.character.weapons[1] = {
+            name: "bow",
+            mod: 2,
+            attr: "dexterity",
+            equipped: true,
+          };
+          break;
+        default:
+          this.character.weapons[1] = {
+            name: "",
+            mod: 0,
+            attr: "",
+            equipped: false,
+          };
+      }
     },
     onSubmit(event) {
       event.preventDefault();
@@ -162,6 +334,10 @@ export default {
   border: 2px solid red;
 }
 img {
-  margin: 50px;
+  margin: 10px;
+}
+
+.center-text {
+  text-align: center;
 }
 </style>
