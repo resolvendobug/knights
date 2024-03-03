6f12a262bc9c8ad0ffd7b78e19b32d09ef59caea
Renato Carvalho <renato@resolvendobug.com.br>
2024-03-03 19:10:11 -0300

    Update vue.config.js with allowed hosts and public URL

--- knights-challenge/vue.config.js
+++ knights-challenge/vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+/*
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    allowedHosts: [
+      'ec2-54-225-42-82.compute-1.amazonaws.com'
+    ],
+    public: 'http://ec2-54-225-42-82.compute-1.amazonaws.com:8080'
+  }
+})
+*/
