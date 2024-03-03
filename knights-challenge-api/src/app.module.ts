3e28529420785809013eb6b87670a42dfb9c1b29
Renato Carvalho <renato@resolvendobug.com.br>
2024-03-03 16:57:31 -0300

    Add @nestjs/config dependency and configure MongoDB connection

--- knights-challenge-api/src/app.module.ts
+++ knights-challenge-api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { KnightsModule } from './knights/knights.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
-  imports: [KnightsModule , MongooseModule.forRoot('mongodb://localhost/knights')],
+  imports: [ConfigModule.forRoot(), KnightsModule, MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [],
   providers: [],
 })
--- knights-challenge-api/src/main.ts
+++ knights-challenge-api/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
@@ -16,6 +17,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 80);
 }
 bootstrap();
